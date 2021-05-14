6ee2271dd8b9b9b976d2323589b61e6c4162e775
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-14 17:52:01 +0300

    add test_selectif_double

--- tests/test_etable.py
+++ tests/test_etable.py
@@ -3,13 +3,15 @@ import os
 
 def test_etable():
     mydir = os.path.dirname(__file__)
-    # file='trucks.xlsx'
-    # file='summm.xlsx'
-    # file = 'plus.xlsx'
-    # file = 'simple_branch.xlsx'
-    # file = 'simple.xlsx'
     file = 'selectfromrange.xlsx'
 
+    et = hyper_etable.etable.ETable(os.path.join(mydir, file), "selectfromrange")
+    et.calculate()
 
-    et = hyper_etable.etable.ETable(os.path.join(mydir, file), "test_etable")
+
+def test_selectif_double():
+    mydir = os.path.dirname(__file__)
+    file = 'selectif_double.xlsx'
+
+    et = hyper_etable.etable.ETable(os.path.join(mydir, file), "selectif_double")
     et.calculate()
