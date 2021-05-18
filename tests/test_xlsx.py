709c8c4d6e76b5f7ecfea08ffe573fb494312ba0
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-18 13:41:23 +0300

    test_solve_xtj pretty print

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -4,8 +4,8 @@ import pytest
 import pathlib
 
 @pytest.mark.timeout(300)
-@pytest.mark.parametrize('xlsx_file', pathlib.Path('./tests/xlsx/').rglob('*.xlsx'))
-def test_solve_xtj(xlsx_file):
+@pytest.mark.parametrize('name, xlsx_file', [(str(p), p) for p in pathlib.Path('./tests/xlsx/').rglob('*.xlsx')])
+def test_solve_xtj(name, xlsx_file):
     print(f"\ntest file {xlsx_file}", end='')
     project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
     et = hyper_etable.etable.ETable(xlsx_file, project_name=project_name)
