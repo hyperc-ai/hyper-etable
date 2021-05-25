883ae8125dc58962c243bab21bb9ef0fccd82377
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-25 18:30:34 +0300

    add simle takeif test

--- tests/test_etable.py
+++ tests/test_etable.py
@@ -4,7 +4,7 @@ import os
 
 def test_etable():
     mydir = os.path.dirname(__file__)
-    file = 'xlsx/HyperC_watchtakeif.xlsm'
+    file = 'xlsx/HyperC_simple_inc.xlsm'
 
     et = hyper_etable.etable.ETable(os.path.join(mydir, file), "watchtakeif")
     et.calculate()
