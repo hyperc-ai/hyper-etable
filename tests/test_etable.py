4cfc5383c24c2bdfa43d06990b0bf64860753fff
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-28 18:40:28 +0300

    fix clean for precondition xlsx/selectfromrange_takeif_twice_synced.xlsx #35

--- tests/test_etable.py
+++ tests/test_etable.py
@@ -4,7 +4,7 @@ import os
 
 def test_etable():
     mydir = os.path.dirname(__file__)
-    file = 'HyperC_tests_simpleinc_log_DOESNT_SOLVE.copy.xlsm'
+    file = 'xlsx/selectfromrange_takeif_twice_synced.xlsx'
 
     et = hyper_etable.etable.ETable(os.path.join(mydir, file), "watchtakeif")
     et.calculate()
