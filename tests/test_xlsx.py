9ece75357ab8355a0e48deae9cb9d63b3ddadd80
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-31 09:25:00 +0300

    add *.xlsm to test_xlsx.py

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,11 +1,11 @@
 """PYTEST_DONT_REWRITE"""
 import hyper_etable.etable
-import os
+import itertools
 import pytest
 import pathlib
 
 @pytest.mark.timeout(300)
-@pytest.mark.parametrize('name, xlsx_file', [(str(p), p) for p in pathlib.Path('./tests/xlsx/').rglob('*.xlsx')])
+@pytest.mark.parametrize('name, xlsx_file', [(str(p), p) for p in itertools.chain(*[pathlib.Path('./tests/xlsx/').rglob(e) for e in ('*.xlsm', '*.xlsx')])])
 def test_solve_xtj(name, xlsx_file):
     print(f"\ntest file {xlsx_file}", end='')
     project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
