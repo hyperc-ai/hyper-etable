71b3b00e064f513ed93756e2a51ba212d73242af
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-31 20:32:28 +0300

    fix test_xlsx.py (disable recursion)

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -3,10 +3,13 @@ import hyper_etable.etable
 import itertools
 import pytest
 import pathlib
+import glob
+import os
 
 @pytest.mark.timeout(300)
-@pytest.mark.parametrize('name, xlsx_file', [(str(p), p) for p in itertools.chain(*[pathlib.Path('./tests/xlsx/').rglob(e) for e in ('*.xlsm', '*.xlsx')])])
+@pytest.mark.parametrize('name, xlsx_file',[(str(p),p)for p in itertools.chain(*[glob.glob(os.path.join('./tests/xlsx/', e)) for e in ('*.xlsm', '*.xlsx', '*.xlsx.txt', '*.xlsx.txt')])])
 def test_solve_xtj(name, xlsx_file):
+    xlsx_file = pathlib.Path(xlsx_file)
     print(f"\ntest file {xlsx_file}", end='')
     project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
     et = hyper_etable.etable.ETable(xlsx_file, project_name=project_name)
