a0254678151f0574b0a960a33e84c821a66e50b0
Kuznetsov Andrey <andreykyz@gmail.com>
2021-05-31 09:47:02 +0300

    fix test_fast (disable recursion)

--- tests/test_fast.py
+++ tests/test_fast.py
@@ -3,6 +3,8 @@ import hyper_etable.etable
 import itertools
 import pytest
 import pathlib
+import glob
+import os
 
 try:
     @pytest.mark.timeout(300)
@@ -10,8 +12,9 @@ try:
                              [(str(p),
                                p)
                               for p in itertools.chain(
-                                  *[pathlib.Path('./tests/').rglob(e) for e in ('*.xlsm', '*.xlsx', '*.xlsx.txt', '*.xlsx.txt')])])
+                                  *[glob.glob(os.path.join('./tests/', e)) for e in ('*.xlsm', '*.xlsx', '*.xlsx.txt', '*.xlsx.txt')])])
     def test_fast(name, xlsx_file):
+        xlsx_file = pathlib.Path(xlsx_file)
         print(f"\ntest file {xlsx_file}", end='')
         project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
         et = hyper_etable.etable.ETable(xlsx_file, project_name=project_name)
