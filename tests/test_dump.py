fc424dd613f1796885fcdb6427a0b3fecb3dbf05
Kuznetsov Andrey <andreykyz@gmail.com>
2021-07-29 14:39:22 +0300

    test

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -41,9 +41,8 @@ def test_dentist():
     print(f"\ntest file {xlsx_file}", end='')
     project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
     et = hyper_etable.etable.ETable(xlsx_file, project_name=project_name)
-    et.solve_dump(has_header=True)
-    et.save_dump(has_header=True)
-
+    et.open_dump(has_header=True)
+    et.dump_py()
 
 def test_space():
     xlsx_file='./tests/space_bug/Untitled 1.xlsx'
