eb1116188ef7660d42aa26c0f5ffe34526d3bc72
Kuznetsov Andrey <andreykyz@gmail.com>
2021-08-03 18:03:56 +0300

    makout previos commit #181 #186

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -2,6 +2,41 @@ import hyper_etable.run_util
 import hyper_etable.etable
 import pathlib
 
+
+def open_test_solve_save_run(input_xlsx, input_py, output_dir_classes, output_dir_plan, output_dir_solution, project_name="etable_project", py_plan_prefix='DATA.', has_header=True):
+    """test for hyper_etable.run_util.open_test_solve_save_run"""
+    et = hyper_etable.etable.ETable(input_xlsx, project_name=project_name)
+    et.open_dump(has_header=has_header, addition_python_files=input_py)
+    et.dump_py(dir=output_dir_classes)
+    assert et.mod.DATA.DATA_79.D == 8
+    et.solver_call_simple_with_exec()
+    assert et.mod.DATA.DATA_79.D == 5
+    et.reset_data()
+    assert et.mod.DATA.DATA_79.D == 8
+    et.solver_call_simple_wo_exec()
+    assert et.mod.DATA.DATA_79.D == 8
+    et.save_plan(prefix=py_plan_prefix, out_dir=output_dir_plan)
+    assert et.mod.DATA.DATA_79.D == 8
+    et.run_plan(et.plan_file)
+    assert et.mod.DATA.DATA_79.D == 5
+    et.save_dump(out_dir=output_dir_solution)
+    assert et.mod.DATA.DATA_79.D == 5
+
+def test_dump():
+    xlsx_file=pathlib.Path('./tests/dump/dump_me.xlsx')
+    out_dir = pathlib.Path('./tests/dump/out/')
+    print(f"\ntest file {xlsx_file}", end='')
+    project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
+    # hyper_etable.run_util.open_test_solve_save_run(
+    open_test_solve_save_run(
+        input_xlsx=[xlsx_file],
+        input_py=['./tests/dump/gg.py'],
+        output_dir_classes=out_dir,
+        output_dir_plan=out_dir,
+        output_dir_solution=out_dir,
+        project_name=project_name,
+        has_header=False)
+
 def test_dump():
     xlsx_file=pathlib.Path('./tests/dump/dump_me.xlsx')
     out_dir = pathlib.Path('./tests/dump/out/')
@@ -15,7 +50,7 @@ def test_dump():
         output_dir_solution=out_dir,
         project_name=project_name,
         has_header=False)
-    
+
 def test_header():
     xlsx_file=pathlib.Path('./tests/header_select/data.xlsx')
     print(f"\ntest file {xlsx_file}", end='')
