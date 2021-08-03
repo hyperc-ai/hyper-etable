5bd30494c165f8277b3448f741fe28c51d772f0e
Kuznetsov Andrey <andreykyz@gmail.com>
2021-08-03 18:46:52 +0300

    del waste code

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -37,21 +37,6 @@ def test_dump():
         project_name=project_name,
         has_header=False)
 
-def test_dump():
-    xlsx_file=pathlib.Path('./tests/dump/dump_me.xlsx')
-    out_dir = pathlib.Path('./tests/dump/out/')
-    print(f"\ntest file {xlsx_file}", end='')
-    project_name = xlsx_file.name.replace("/", "_").replace(".", "_")
-    hyper_etable.run_util.open_test_solve_save_run(
-        input_xlsx=[xlsx_file],
-        input_py=['./tests/dump/gg.py'],
-        output_dir_classes=out_dir,
-        output_dir_plan=out_dir,
-        output_dir_solution=out_dir,
-        project_name=project_name,
-        has_header=False)
-
-
 def test_header():
     xlsx_file=pathlib.Path('./tests/header_select/has_header.xlsx')
     out_dir = pathlib.Path('./tests/header_select/out/')
