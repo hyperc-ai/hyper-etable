94c4eaa7f2064560994c5b07b181da8c0ea3bde2
Kuznetsov Andrey <andreykyz@gmail.com>
2021-08-05 15:35:13 +0300

    add new row test #183

---
+++ tests/add_row/a.py
@@ -0,0 +1,13 @@
+def aa(gg:SHEET1_Class):
+    assert gg.HEADER2 == 'bad'
+    gg.HEADER1 = 1
+    gg.HEADER2 = 'good100500'
+
+def real_good(gg:SHEET1_Class):
+    assert gg.HEADER2 == 'good100500'
+    gg.HEADER1 = 2
+    gg.HEADER2 = 'ooh'
+    new_row=SHEET1_Class()
+    new_row.HEADER1 = 7
+    new_row.HEADER2 = 'new row'
+    DATA.GOAL=True    
