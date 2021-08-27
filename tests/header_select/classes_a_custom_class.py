b5f0f2739eef1eedc5765dc49cf7471caacb6057
Kuznetsov Andrey <andreykyz@gmail.com>
2021-08-27 16:14:48 +0300

    add lost files

---
+++ tests/header_select/classes_a_custom_class.py
@@ -0,0 +1,37 @@
+class TableElementMeta(type):
+    pass
+    def __init__(self):
+        pass
+
+class SHEET1_Class:
+    __table_name__: str
+    addidx: int
+    HEADER1: str
+    HEADER2: str
+    def __init__(self):
+        self.__table_name__ = "SHEET1"
+        self.__xl_sheet_name__ = "Sheet1"
+
+class StaticObject:
+    SHEET1_1: SHEET1_Class
+    SHEET1_2: SHEET1_Class
+    SHEET1_3: SHEET1_Class
+    SHEET1_4: SHEET1_Class
+    SHEET1_5: SHEET1_Class
+    SHEET1_6: SHEET1_Class
+    SHEET1_7: SHEET1_Class
+    SHEET1_8: SHEET1_Class
+    SHEET1_9: SHEET1_Class
+    SHEET1_10: SHEET1_Class
+    SHEET1_11: SHEET1_Class
+    SHEET1_addidx: int
+    GOAL: bool
+    def __init__(self):
+        pass
+
+class DefinedTables:
+    pass
+    def __init__(self):
+        pass
+
+DATA = StaticObject()
