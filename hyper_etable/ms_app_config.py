7e246e55f444334a5658f32d96d00df21211fd25
Kuznetsov Andrey <andreykyz@gmail.com>
2021-09-10 18:46:24 +0300

    add ms sheet api

---
+++ hyper_etable/ms_app_config.py
@@ -0,0 +1,9 @@
+import os
+
+CLIENT_ID = "1453bd78-dc7f-47c9-b537-37b01aef589d"
+CLIENT_SECRET = "z~F.kQ~V2QSFaCA0vvIBs7_4Ks3-hiekiL"
+
+AUTHORITY = "https://login.microsoftonline.com/common"
+SCOPE = ["Files.Read", "Files.Read.All", "Files.ReadWrite", "Files.ReadWrite.All", "User.ReadBasic.All"]
+SESSION_TYPE = "filesystem"
+REDIRECT_PATH = "/getAToken"
