bc88e7853f700c4dc126064c86933e1d99803357
Kuznetsov Andrey <andreykyz@gmail.com>
2021-09-20 18:44:19 +0300

    add lost file #222

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -4,7 +4,8 @@ import hyper_etable.etable
 def test_gsheet():
     file_id = '1IGqkZK3yOvejLFBSjTPxrwSkJDZLjX-ibnGjAq6SAps'
     et = hyper_etable.etable.ETable(project_name='test_custom_class_edited')
-    et.open_from(path=file_id, has_header=True, proto='gsheet')
-    path = ['appZ5mJvdfY2ZHzzw','key3hulhtlvhMFtQ8', 'Epics']
-    et.open_from(path=path, proto='airtable')
-    print("ok")
+    gconn=et.open_from(path=file_id, has_header=False, proto='gsheet')
+    gconn.save()
+    # path = ['appZ5mJvdfY2ZHzzw','key3hulhtlvhMFtQ8', 'Epics']
+    # et.open_from(path=path, proto='airtable')
+    # print("ok")
